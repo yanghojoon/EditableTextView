5a0078db533749ba0e6c0c441aec950c2154cae3
yanghojoon <yanghojoon8487@gmail.com>
2025-05-15 17:26:28 +0900

    feat: TextView Long Press 시 메뉴에 Bold 적용 버튼 추가

--- EditableTextView/ViewController.swift
+++ EditableTextView/ViewController.swift
@@ -16,6 +16,7 @@ final class ViewController: UIViewController {
         textView.layer.borderWidth = 1
         textView.layer.cornerRadius = 5
         textView.clipsToBounds = true
+        textView.font = UIFont.systemFont(ofSize: 14)
         return textView
     }()
 
@@ -28,6 +29,11 @@ final class ViewController: UIViewController {
 
     private func setAttributes() {
         view.backgroundColor = .white
+        setTextViewAttributes()
+    }
+
+    private func setTextViewAttributes() {
+        textView.delegate = self
     }
 
     private func configureSubviewLayout() {
@@ -35,6 +41,41 @@ final class ViewController: UIViewController {
 
         textView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide).inset(10)
+        }
+    }
+}
+
+extension ViewController: UITextViewDelegate {
+    func textView(
+        _ textView: UITextView,
+        editMenuForTextIn range: NSRange,
+        suggestedActions: [UIMenuElement]
+    ) -> UIMenu? {
+        var additionalActions = [UIMenuElement]()
+
+        if range.length > 0 {
+            let boldAction = UIAction(title: "Bold") { [weak self] _ in
+                self?.applyAttribute(.font, value: UIFont.boldSystemFont(ofSize: 14))
+            }
+
+            additionalActions.append(boldAction)
+        }
+
+        return UIMenu(children: suggestedActions + additionalActions)
+    }
+
+    private func applyAttribute(_ key: NSAttributedString.Key, value: Any) {
+        guard let selectedRange = textView.selectedTextRange else { return }
+
+        let nsRange = NSRange(
+            location: textView.offset(from: textView.beginningOfDocument, to: selectedRange.start),
+            length: textView.offset(from: selectedRange.start, to: selectedRange.end)
+        )
+
+        if nsRange.length > 0 {
+            let mutableAttributedString = NSMutableAttributedString(attributedString: textView.attributedText)
+            mutableAttributedString.addAttribute(key, value: value, range: nsRange)
+            textView.attributedText = mutableAttributedString
         }
     }
 }
